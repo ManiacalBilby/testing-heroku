a8ae25bd1d4a50fecad1229a4c0d0c43c0407e9b
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-02 15:30:46 -0500

    fixed bug

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI); 
 
-mongoose.once('open', () => {
+mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB!!!')
 })
 
